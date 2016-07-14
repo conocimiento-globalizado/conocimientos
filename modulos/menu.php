4cee8c07f8997edbcfdcd5b727e54549d1adf71c
Isaac La Serna Castillo <iclasac@gmail.com>
2016-07-14 00:13:01 -0500

    Cambios en estilos y agregamos un enlace mas al menu de nombre Manual

--- modulos/menu.php
+++ modulos/menu.php
@@ -2,4 +2,5 @@
 			<li><a href="index.php">Inicio</a></li>
 			<li><a href="convertidor.php">Convertidor</a></li>
 			<li><a href="formulario.php">Formulario</a></li>
+			<li><a href="manual.php">Manual</a></li>
 		</ul>
