848dc9381b0e9cb477d22bb05cceda0f24422cb6
Isaac La Serna Castillo <iclasac@gmail.com>
2016-07-16 01:02:00 -0500

    cambiando h1 de mi esto

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <body>
 <div class="contenedor">
 	<header>
-		<h1>Sitio Web</h1>
+		<h1>Sitio Web de ISAAC</h1>
 		<p>Aplicando conocimiento</p>
 		<!-- comienza el menu -->
 		<?php include "modulos/menu.php"; ?>
